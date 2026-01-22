056936ff924f7f612c1cf6ad8164269738d7e4ef
norvik <ai.norvik@gmail.com>
2026-01-22 23:09:47 +0400

    Add general utilities module init

---
+++ src/general_utils/__init__.py
@@ -0,0 +1,8 @@
+"""General utilities for configuration and common operations."""
+
+from src.general_utils.config_loader import load_yaml_config, validate_config
+
+__all__ = [
+    "load_yaml_config",
+    "validate_config",
+]
