cda22c5feed1d375da726b3a202e630981eaed10
norvik <ai.norvik@gmail.com>
2026-01-22 23:33:16 +0400

    Update data_prep exports

--- src/data_prep/__init__.py
+++ src/data_prep/__init__.py
@@ -2,6 +2,13 @@
 
 from .chunker import chunk_documents
 from .downloader import download_docs
+from .preprocessor import preprocess_all_docs
 from .vectorstore import create_vectorstore, load_vectorstore
 
-__all__ = ["download_docs", "chunk_documents", "create_vectorstore", "load_vectorstore"]
+__all__ = [
+    "download_docs",
+    "preprocess_all_docs",
+    "chunk_documents",
+    "create_vectorstore",
+    "load_vectorstore",
+]
